22f944ff50c7ad5efffbb1b8675a21fa10352b3e
youssef-terhmina <terhminayoussef@gmail.com>
2023-05-24 16:12:30 -0700

    Ur turn bro

--- simple.c
+++ simple.c
@@ -1,10 +1,12 @@
 #include "head.h"
+
 /**
  * simples - function to simplify the string
  * @buff: buffer
  * @d: delimiters
  * Return: each part of string inputed
  */
+
 char **simples(char *buff, char *d)
 {
 	char **parts = malloc(sizeof(char *) * 1024); /* each token of string */
