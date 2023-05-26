9674c4318c824b44c5c62fe3aa795009154b98e1
youssef-terhmina <terhminayoussef@gmail.com>
2023-05-26 07:37:27 -0700

    Ur turn bro

---
+++ builtin2.c
@@ -0,0 +1,28 @@
+#include "head.h"
+
+/**
+ * builtin_cd - changes directory
+ * @d: data struct input
+ * Return: nothing
+ */
+
+void builtin_cd(data *d)
+{
+	char *dir = d->av[1];
+	char cwd[256];
+
+	if (getcwd(cwd, sizeof(cwd)) == NULL)
+		perror("getcwd() error");
+	_setenv(d, "PWD", cwd);
+	if (!dir)
+		dir = _getenv("HOME");
+	if (chdir(dir) == -1)
+		perror("cd");
+	else
+	{
+		_setenv(d, "OLDPWD", _getenv("PWD"));
+		if (getcwd(cwd, sizeof(cwd)) == NULL)
+			perror("getcwd() error");
+		_setenv(d, "PWD", cwd);
+	}
+}
