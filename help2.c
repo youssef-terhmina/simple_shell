1d3ea2de1b8b74cd89e41c3ed25eacdb0d1ba78e
youssef-terhmina <terhminayoussef@gmail.com>
2023-05-26 17:58:29 -0700

    Ur turn bro

--- help2.c
+++ help2.c
@@ -2,27 +2,28 @@
 
 /**
  * _perror - print in stderr
- * @str1: name of shell program
- * @str2: message error
- * Return: void
+ * @str: name of shell program
+ * @string: message error
+ * Return: nothing
  */
-void _perror(const char *str1, const char *str2)
+
+void _perror(const char *str, const char *string)
 {
-	if (!str1 || !str2)
+	if (!str || !string)
 		return;
 
-	while (*str1)
+	while (*str)
 	{
-		write(STDERR_FILENO, str1, 1);
-		str1++;
+		write(STDERR_FILENO, str, 1);
+		str++;
 	}
 
 	write(STDERR_FILENO, ": ", 2);
 
-	while (*str2)
+	while (*string)
 	{
-		write(STDERR_FILENO, str2, 1);
-		str2++;
+		write(STDERR_FILENO, string, 1);
+		string++;
 	}
 	write(STDERR_FILENO, "\n", 1);
 }
@@ -31,57 +32,56 @@ void _perror(const char *str1, const char *str2)
 /**
  * _trim - remove leading and trailing spaces and tabs from a string
  * @str: string input
- * Return: void.
+ * Return: nothing
  */
 
 void _trim(char *str)
 {
-	int i, j, len = _strlen(str);
+	int x, y, len = _strlen(str);
 
-	for (i = 0; i < len && (str[i] == ' ' || str[i] == '\t'); i++)
+	for (x = 0; x < l && (str[x] == ' ' || str[x] == '\t'); x++)
 		;
 
-	for (j = 0; i < len ; i++, j++)
-		str[j] = str[i];
+	for (y = 0; x < l ; x++, y++)
+		str[y] = str[x];
 
-	str[j] = '\0';
+	str[y] = '\0';
 
-	for (i = _strlen(str) - 1; i > 0 && (str[i] == ' ' || str[i] == '\t'); i--)
-		str[i] = '\0';
+	for (x = _strlen(str) - 1; x > 0 && (str[x] == ' ' || str[x] == '\t'); x--)
+		str[x] = '\0';
 }
 
 
 /**
  * _realloc - reallocates a memory block using malloc and free
- *
  * @ptr: pointer void
- * @new_size: unsigned int
- *
- * Return: the newly allocated memory
+ * @nsize: unsigned int
+ * Return: new allocated memory
  */
-void *_realloc(void *ptr, unsigned int new_size)
+
+void *_realloc(void *ptr, unsigned int nsize)
 {
 	char *p;
-	unsigned int i, tp = new_size, old_size = sizeof(ptr);
+	unsigned int x, tp = nsize, osize = sizeof(ptr);
 
-	if (old_size == new_size)
+	if (osize == nsize)
 		return (ptr);
 	if (ptr == NULL)
-		return (malloc(new_size));
+		return (malloc(nsize));
 
-	if (new_size > old_size)
-		tp = old_size;
+	if (nsize > osize)
+		tp = osize;
 
-	if (new_size == 0)
+	if (nsize == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
 
-	p = malloc(new_size);
+	p = malloc(nsize);
 
-	for (i = 0; i < tp; i++)
-		p[i] = ((char *)ptr)[i];
+	for (x = 0; x < tp; x++)
+		p[x] = ((char *)ptr)[x];
 	free(ptr);
 	return (p);
 }
